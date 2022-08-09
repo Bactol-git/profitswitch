60f87c36b653ff7967ba54cb2af670e1b1ff6970
Bactol-git <110667350+Bactol-git@users.noreply.github.com>
2022-08-09 23:49:51 +0200

    fixed issue with prompting for pw when switching to miners run as sudo

--- profitswitch.py
+++ profitswitch.py
@@ -171,28 +171,9 @@ if highest_profit >= 0.01:
         wd = wd.strip()
         try:
             os.system(start_OC)
-            subprocess.Popen([start_miner], shell=False, cwd=wd)
+            subprocess.Popen(['sudo',start_miner], shell=False, cwd=wd)
         except Exception as e:
-            print(e)
-
-        time.sleep(20)
-
-        with open('current_algo', 'w') as f:
-            f.write(new_algo)
-        
-        with open('PID_to_kill') as file:
-            find_PID = file.readlines() 
-
-        find_PID = find_PID[0]
-        find_PID = find_PID.replace('\n', '')
-
-        process = subprocess.run(find_PID, capture_output=True, text=True, shell=True)
-        process_to_kill = process.stdout
-
-        if process_to_kill == '':
-            subprocess.Popen([start_miner], shell=False, cwd=wd)
-        else:
-            pass       
+            print(e)      
     
     else:
         with open('PID_to_kill') as file:
@@ -205,6 +186,6 @@ if highest_profit >= 0.01:
         process_to_kill = process.stdout
 
         if process_to_kill == '':
-            subprocess.Popen([start_miner], shell=False, cwd=wd)
+            subprocess.Popen(['sudo',start_miner], shell=False, cwd=wd)
 else:
     new_algo = 'Better off not mining'
