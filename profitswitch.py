b16b182d1a84ffd1c0c97132e4691ba42610dfe8
Bactol-git <110667350+Bactol-git@users.noreply.github.com>
2022-08-09 11:35:33 +0200

    Update profitswitch.py

--- profitswitch.py
+++ profitswitch.py
@@ -193,6 +193,16 @@ if highest_profit >= 0.01:
             pass       
     
     else:
-        pass
+        with open('PID_to_kill') as file:
+            find_PID = file.readlines() 
+
+        find_PID = find_PID[0]
+        find_PID = find_PID.replace('\n', '')
+
+        process = subprocess.run(find_PID, capture_output=True, text=True, shell=True)
+        process_to_kill = process.stdout
+
+        if process_to_kill == '':
+            subprocess.Popen([start_miner], shell=False, cwd=wd)
 else:
     new_algo = 'Better off not mining'
