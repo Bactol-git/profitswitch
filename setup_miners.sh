1ec44f8fa6578e7153dcdb55e138c26ad9c2b4e9
Bactol-git <110667350+Bactol-git@users.noreply.github.com>
2022-09-01 21:13:03 +0200

    Update setup_miners.sh

--- setup_miners.sh
+++ setup_miners.sh
@@ -11,9 +11,7 @@ rm miniZ*
 #lolminer
 mkdir "$(pwd)/miners/lolminer"
 lolminer=$(curl https://api.github.com/repos/Lolliedieb/lolMiner-releases/releases/latest| jq -r ".assets[] | select(.name | test(\"_Lin64.tar.gz\")) | .browser_download_url")
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.54/lolMiner_v1.54_Lin64.tar.gz
 wget $lolminer
-rm *.tar.gz.1
 tar -xf lolMiner* --dir="$(pwd)/miners/lolminer" --strip-components=1
 rm lolMiner*
 
