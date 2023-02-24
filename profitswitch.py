31042a7f5258ab65dfedda9d68af1771aab8de9e
Bactol-git <110667350+Bactol-git@users.noreply.github.com>
2023-02-24 12:52:08 +0100

    updated cfx reward

--- profitswitch.py
+++ profitswitch.py
@@ -202,7 +202,7 @@ else:
             cfx_diff = 0
 
         cfx_block_time = 0.5
-        cfx_block_reward = 2
+        cfx_block_reward = 1
 
         try:
             flux_price = float(price_temp['flux']['data']['c'])
